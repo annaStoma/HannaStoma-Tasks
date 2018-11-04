1daa54d65d62a7beb2ca6006161a1af16b32c077
Hanna Stoma <annastoma@inbox.ru>
2018-11-04 12:46:35 +0300

    fixed naming and "return"

--- task_4/script.js
+++ task_4/script.js
@@ -1,68 +1,68 @@
 console.log('task 1 ---------------------');
 //Найти все простые числа до заданного n.
 //Простые числа - это те числа, которые делятся
-//только на себя и на 1. 1 <= n 1 000 000 000
-function task1(n) {
-    var i;
+//только на себя и на 1. 1 <= n 1 000 000 000  \
+function simpleNumbers(n) {
     var summ = 0;
-    for (i = 1; i < n; i++) {
-        for (m = 1; m < i; m++) {
+    for ( var i = 1; i < n; i++)
+    {
+        for (m = 1; m < i; m++)
+        {
             if (i % m == 0)
                 i++;
         }
         summ++; 
     }
-    console.log("The number of prime numbers from 1 to " + n + " is " + summ);
-    return;
+    return summ;
 }
-task1(100);
-console.log('task 2 ---------------------');
+console.log("The number of prime numbers is " + simpleNumbers(50));
+   
+console.log('task 2 ------------------');
 //Найти первые n простых чисел.
 //1 =< n <= 1000000
-function task2(n) {
-    arr = [];
-    var i;
-    var count = 0;
-    console.log('First ' + n + ' prime numbers:');
-    for (i = 1; i < n; i++) {
-        for (m = 1; m < i; m++) {
-            if (i % m == 0)
-                i++;
-        }
-        count++;
-        console.log(count + " : " + i);
+function firstSimpleNumbers(n) {
+    numbers = []; 
+    var count = 1;
+    if (count <= n) {
+        for ( var i = 1; i < 10; i++) {
+            for (m = 1; m < i; m++) {
+                if (i % m == 0)
+                    i++;
+            }
+            console.log(count + " : " + i);
+            count++;
+        } 
+        return numbers;
     }
-    return;
 }
-task2(20);
+console.log('First 20 prime numbers:');
+console.log(firstSimpleNumbers(20));
 
 console.log('task 3 ---------------------');
 //Найти сумму первых n четных чисел фибоначчи. 
 //Числа фибоначчи - первые два числа равны 1 и 1,
 //а каждое последующее число равно сумме
 //двух предыдущих чисел. 1 <= n <= 100000
-function task3(d) {
-    var myArr = new Array(d);
+function fibSumm(d) {
+    var fibNumbers = new Array(d);
     var summ = 0;
-    for (var i = 2; i <= myArr.length - 1; i++) {
-        myArr[0] = myArr[1] = 1;
-        myArr[i] = myArr[i - 2] + myArr[i - 1];
-        if (myArr[i] % 2 == 0 && myArr[i] !== 1) {
-            summ += myArr[i];
+    for (var i = 2; i <= fibNumbers.length - 1; i++) {
+        fibNumbers[0] = fibNumbers[1] = 1;
+        fibNumbers[i] = fibNumbers[i - 2] + fibNumbers[i - 1];
+        if (fibNumbers[i] % 2 == 0 && fibNumbers[i] !== 1) {
+            summ += fibNumbers[i];
         }
     }
-    console.log('Первые ' + d + ' чисел фибоначи: ' + myArr);
-    console.log('Сумма четных из них = ' + summ);
-    return;
+    return summ;
 }
-task3(8);
+console.log('Сумма четных  = ' + fibSumm(10));
 
 console.log('task 4 ---------------------');
 // Серия чисел, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317 
 // (^ - степень числа). Найдите 10 последних цифр
 //  результата такой последовательности, для заданного n. 
 //  1 <= n <= 1000
-function task4(n) {
+function lastTenNumbers(n) {
     var summ = 0;
     var i = 1;
     for (i = 1; i <= n; i++) {
@@ -71,15 +71,13 @@ function task4(n) {
     }
     console.log("Сумма: " + summ);
     var s = '' + summ;
-    var stroka = s.substring(s.length - 10);
-    console.log('10 последних цифр суммы: ' + stroka);
-    return;
+    var stringOfNumbers = s.substring(s.length - 10);
+    return stringOfNumbers;
 }
-task4(15);
-
+console.log('10 последних цифр суммы: ' + lastTenNumbers(10));
 console.log('task 5 ---------------------');
 
-function task5() {
+function Average(a,b,c,d,e) {
     // Найти среднее арифметическое чисел из массива.
     // [1, 2, 3, 4, 5]
     var summ = 0;
@@ -87,18 +85,15 @@ function task5() {
         summ += arguments[i];
     }
     var result = summ / arguments.length;
-    console.log('Среднее арифметическое:' + result);
-    return;
+    return result;
 }
-task5(1, 2, 3, 4, 5);
-
+console.log('Среднее арифметическое:' + Average(1, 2, 3, 4, 5));
 console.log('task 6 ---------------------');
 // Найти максимальное число в массиве.
 // [1, 2, 3, 4, 5]
-function task6() {
+function maxNumber(a,b,c,d,e) {
     var i = 0;
     for (i = 0; i <= arguments.length - 1; i++) {
-        console.log('Элемент ' + i + ': ' + arguments[i]);
     }
     var max = arguments[0];
     for (i = 0; i <= arguments.length - 1; i++) {
@@ -106,51 +101,54 @@ function task6() {
             arguments[0] = arguments[i];
         }
     }
-
-    console.log('Максимальное:' + arguments[0]);
-    return;
+    return arguments[0];
 }
-task6(1, 2, 3, 4, 5);
-
+console.log('Max is :' +  maxNumber(1, 2, 3, 4, 5));
 console.log('task 7 ---------------------');
 // Найдите все уникальные строки в массиве.
 // [1, 'word', 1, 'word', 'word', 'word1']
 // [1, 'word', 'word1']
-function task7() {
+function uniqueString(a,b,c,d,e,s) {
     var myArray = [];
     for (var i = 0; i < arguments.length; i++) {
         if (myArray.indexOf(arguments[i]) == -1)
             myArray.push(arguments[i]);
     }
-    console.log(myArray);
-    return;
+    return myArray;
 }
-task7(1, 'word', 1, 'word', 'word', 'word1');
-
+console.log("уникальные строки в массиве: [1, 'word', 1, 'word', 'word', 'word1']  это " + uniqueString(1, 'word', 1, 'word', 'word', 'word1'));
 console.log('task 8 ---------------------');
 // Определить, является ли фраза/слово палиндромом
 // ДОХОД
 // ИСТИНА
-function task8(word) {
+function isPalindrome(word) {
     var wordSplit = word.split('');
     var wsr = wordSplit.reverse().join('');
-    //alert( str.split('') ); // т,е,с,т
-    console.log(word == wsr);
-    return word;
+    if (word == wsr)
+    {
+        return true;
+        console.log("dfdfdf");
+    }
+    else 
+    {
+        return false;
+    }
 }
-task8('доход');
+console.log('Слово полином: ' + isPalindrome('доход'));
+ 
 
 console.log('task 9 ---------------------');
 // Найти сумму цифр данного числа
 // "564674474"
 // 47
-function task9(n) {
+function summOfNumbers(n) {
     var stroka = String(n).split('');
     var summ = 0;
     for (i = 0; i <= stroka.length - 1; i++) {
         summ += Number(stroka[i]);
     }
-    console.log(summ);
-    return;
+    return summ;
 }
-task9(564674474);
+console.log('сумма цифр = ' + summOfNumbers(564674474));
+ 
+ 
