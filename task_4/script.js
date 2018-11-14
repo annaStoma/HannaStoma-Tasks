daa9e6b4bf2593d14e1154f17ec937aa2908dc11
Hanna Stoma <annastoma@inbox.ru>
2018-11-14 21:41:20 +0300

    fixed

--- task_4/script.js
+++ task_4/script.js
@@ -1,7 +1,4 @@
 console.log('task 1 ---------------------');
-//Найти все простые числа до заданного n.
-//Простые числа - это те числа, которые делятся
-//только на себя и на 1. 1 <= n 1 000 000 000  \
 function simpleNumbers(n) {
     var summ = 0;
     for ( var i = 1; i < n; i++)
@@ -18,8 +15,6 @@ function simpleNumbers(n) {
 console.log("The number of prime numbers is " + simpleNumbers(50));
    
 console.log('task 2 ------------------');
-//Найти первые n простых чисел.
-//1 =< n <= 1000000
 function firstSimpleNumbers(n) {
     numbers = []; 
     var count = 1;
@@ -29,7 +24,6 @@ function firstSimpleNumbers(n) {
                 if (i % m == 0)
                     i++;
             }
-            console.log(count + " : " + i);
             count++;
         } 
         return numbers;
@@ -39,10 +33,6 @@ console.log('First 20 prime numbers:');
 console.log(firstSimpleNumbers(20));
 
 console.log('task 3 ---------------------');
-//Найти сумму первых n четных чисел фибоначчи. 
-//Числа фибоначчи - первые два числа равны 1 и 1,
-//а каждое последующее число равно сумме
-//двух предыдущих чисел. 1 <= n <= 100000
 function fibSumm(d) {
     var fibNumbers = new Array(d);
     var summ = 0;
@@ -58,10 +48,6 @@ function fibSumm(d) {
 console.log('Сумма четных  = ' + fibSumm(10));
 
 console.log('task 4 ---------------------');
-// Серия чисел, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317 
-// (^ - степень числа). Найдите 10 последних цифр
-//  результата такой последовательности, для заданного n. 
-//  1 <= n <= 1000
 function lastTenNumbers(n) {
     var summ = 0;
     var i = 1;
@@ -69,17 +55,14 @@ function lastTenNumbers(n) {
         var mult = Math.pow(i, i);
         summ += mult;
     }
-    console.log("Сумма: " + summ);
     var s = '' + summ;
     var stringOfNumbers = s.substring(s.length - 10);
     return stringOfNumbers;
 }
 console.log('10 последних цифр суммы: ' + lastTenNumbers(10));
-console.log('task 5 ---------------------');
 
+console.log('task 5 ---------------------');
 function Average(a,b,c,d,e) {
-    // Найти среднее арифметическое чисел из массива.
-    // [1, 2, 3, 4, 5]
     var summ = 0;
     for (var i = 0; i <= arguments.length - 1; i++) {
         summ += arguments[i];
@@ -88,46 +71,39 @@ function Average(a,b,c,d,e) {
     return result;
 }
 console.log('Среднее арифметическое:' + Average(1, 2, 3, 4, 5));
+
 console.log('task 6 ---------------------');
-// Найти максимальное число в массиве.
-// [1, 2, 3, 4, 5]
-function maxNumber(a,b,c,d,e) {
-    var i = 0;
-    for (i = 0; i <= arguments.length - 1; i++) {
+function maxNumber(myArr) {
+    for ( var i = 0; i <= myArr.length - 1; i++) {
     }
-    var max = arguments[0];
-    for (i = 0; i <= arguments.length - 1; i++) {
-        if (arguments[i] > arguments[0]) {
-            arguments[0] = arguments[i];
+    var max = myArr[0];
+    for (i = 0; i <= myArr.length - 1; i++) {
+        if (myArr[i] > myArr[0]) {
+            myArr[0] = myArr[i];
         }
     }
-    return arguments[0];
+    return myArr[0];
 }
-console.log('Max is :' +  maxNumber(1, 2, 3, 4, 5));
+console.log('Max is :' +  maxNumber([1, 2, 3, 4, 5]));
+
 console.log('task 7 ---------------------');
-// Найдите все уникальные строки в массиве.
-// [1, 'word', 1, 'word', 'word', 'word1']
-// [1, 'word', 'word1']
-function uniqueString(a,b,c,d,e,s) {
+function uniqueString(arr) {
     var myArray = [];
-    for (var i = 0; i < arguments.length; i++) {
-        if (myArray.indexOf(arguments[i]) == -1)
-            myArray.push(arguments[i]);
+    for (var i = 0; i < arr.length; i++) {
+        if (myArray.indexOf(arr[i]) == -1)
+            myArray.push(arr[i]);
     }
     return myArray;
 }
-console.log("уникальные строки в массиве: [1, 'word', 1, 'word', 'word', 'word1']  это " + uniqueString(1, 'word', 1, 'word', 'word', 'word1'));
+console.log("уникальные строки в массиве: [1, 'word', 1, 'word', 'word', 'word1']  это " + uniqueString([1, 'word', 1, 'word', 'word', 'word1']));
+
 console.log('task 8 ---------------------');
-// Определить, является ли фраза/слово палиндромом
-// ДОХОД
-// ИСТИНА
 function isPalindrome(word) {
     var wordSplit = word.split('');
     var wsr = wordSplit.reverse().join('');
     if (word == wsr)
     {
         return true;
-        console.log("dfdfdf");
     }
     else 
     {
@@ -135,12 +111,7 @@ function isPalindrome(word) {
     }
 }
 console.log('Слово полином: ' + isPalindrome('доход'));
- 
-
 console.log('task 9 ---------------------');
-// Найти сумму цифр данного числа
-// "564674474"
-// 47
 function summOfNumbers(n) {
     var stroka = String(n).split('');
     var summ = 0;
